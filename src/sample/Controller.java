e0c0acf2a40c7c25aef720992bd117b2c7cff67a
rishabh0417 <rishabh0417@github.com>
2018-10-30 01:39:50 +0530

    added functionality for opening one window from another and pass some data to the other window with a global variable.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,21 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller {
+
+    static String name = "XXX";
+    @FXML
+    private Label userName;
 
     @FXML
     private TextField usr;
@@ -28,11 +37,27 @@ public class Controller {
     @FXML
     private void initialize(){}
 
-    public Controller(){}
+    public Controller(){
+        System.out.println("first controller" +
+                " ");
+    }
 
     @FXML
     private void Clicked(){
-        usr.setText("Login done");
+//        usr.setText("Login done");
+        String s1 = usr.getText();
+        try {
+            Controller.name = s1;
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("EndUserView.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("EndUserView.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 650, 500));
+            Controller.name = s1;
+            stage.show();
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
+
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,16 +1,20 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginView.fxml"));
+        Parent root = loader.load();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 650, 500));
         primaryStage.show();
@@ -18,6 +22,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
