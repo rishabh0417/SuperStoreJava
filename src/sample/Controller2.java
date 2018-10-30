f4a7b4ceada5fc593efe36700c4ef6c45d773014
rishabh0417 <rishabh0417@github.com>
2018-10-30 18:20:50 +0530

    added EndUserAddBudgetView.fxml

--- src/sample/Controller2.java
+++ src/sample/Controller2.java
@@ -1,7 +1,11 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class Controller2 {
 
@@ -17,5 +21,31 @@ public class Controller2 {
     @FXML
     private void initialize(){
         userName.setText(Controller.name);
+    }
+
+    @FXML
+    private void openCart(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("EndUserCartView.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 650, 500));
+            stage.show();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    private void addMoney(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("EndUserAddBudgetView.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 300, 300));
+            stage.show();
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
