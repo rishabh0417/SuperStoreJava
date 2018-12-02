dd36a593e2fd9d4fab7a5f7902fe094a0dfe4bca
rishabh0417 <rishabh0417@github.com>
2018-12-02 16:44:32 +0530

    Delete for store implemented

---
+++ src/Frontend/StoreDeleteController.java
@@ -0,0 +1,92 @@
+package Frontend;
+
+import Backend.Category;
+import Backend.FileWriter;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreDeleteController {
+    private String current_instance;
+
+    Stage stage;
+
+    @FXML
+    private Button open_up_rec;
+    @FXML private Button add_cat_rec;
+
+    Category curr;
+
+    @FXML
+    private ListView list_categories;
+
+
+    @FXML public void initialize(){
+        stage = new Stage();
+        System.out.println("here");
+        Category curr_category = DATA.currentCategory;
+
+        list_categories.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue observable, String oldValue, String newValue) {
+                DATA.currentCategoryString = newValue;
+                DATA.currentCategory = curr_category.getSubCategory().get(DATA.currentCategoryString);
+//                Product exists in it and not a category so disable the button
+
+                if (DATA.currentCategory.getProduct() != null) open_up_rec.setDisable(true);
+
+//                System.out.println(DATA.path);
+                current_instance = DATA.path;
+            }
+        });
+
+        List<String> mainList = new ArrayList<String>();
+        mainList.addAll(curr_category.getSubCategory().keySet());
+        list_categories.setItems(FXCollections.observableList(mainList));
+
+    }
+
+    @FXML public void open_next_cat(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("StoreDelete.fxml"));
+            Parent root = loader.load();
+            stage = new Stage();
+            stage.setScene(new Scene(root, 650, 500));
+            stage.show();
+            DATA.path = DATA.path + ">" + DATA.currentCategoryString;
+
+            System.out.println(DATA.path);
+
+            stage.setOnCloseRequest(event -> {
+                DATA.path = current_instance;
+                System.out.println("Windows has been closed.");
+            });
+
+        } catch (IOException e){
+            System.out.println(e.getStackTrace());
+        }
+    }
+
+    @FXML public void set_category(){
+
+        System.out.println("The value of current Instance.");
+        System.out.println(current_instance+">"+DATA.currentCategoryString);
+
+        try {
+            DATA.cur_storeAdmin.getStore().store_inventory.delete(current_instance+">"+DATA.currentCategoryString);
+            FileWriter.Serialize(DATA.superStore);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/Frontend/storeAdminController.java
+++ src/Frontend/storeAdminController.java
@@ -2,6 +2,7 @@ package Frontend;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
@@ -14,8 +15,48 @@ public class storeAdminController {
     @FXML private RadioButton radio_category_add;
     @FXML private RadioButton radio_product_add;
 
+    @FXML private RadioButton radio_delete_product;
+    @FXML private RadioButton radio_delete_Category;
+    @FXML private TextField del_text;
+
 
     @FXML private TextField update_text;
+
+    @FXML public void store_del_items(){
+        DATA.string2 = del_text.getText();
+
+
+        if (radio_delete_Category.isSelected()) {
+            DATA.currentCategory = DATA.cur_storeAdmin.getStore().store_inventory.getCategories().get("root");
+            DATA.currentCategoryString = "root";
+            DATA.path = DATA.currentCategoryString;
+
+
+            try {
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("StoreDelete.fxml"));
+                Parent root = loader.load();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root, 650, 500));
+                stage.setTitle("Delete...");
+                stage.show();
+            } catch (IOException e) {
+                System.out.println(e.getCause());
+            }
+        } else if (radio_delete_product.isSelected()){
+            del_text.setDisable(false);
+            Stage stage = new Stage();
+            stage.setTitle("Search Results");
+            try {
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("StoreDeleteSearch.fxml")), 700, 500));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            stage.show();
+        }
+
+    }
 
 
     @FXML public void update_btn_store(){
